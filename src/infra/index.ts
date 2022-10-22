76449b88af3518225a0a805c51cd4b2b862f7fef
Spencer Hachmeister <tms@spencerslab.com>
2022-10-22 15:03:47 -0600

    Tweaks needed for first deployment

--- src/infra/index.ts
+++ src/infra/index.ts
@@ -1,15 +1,8 @@
 import * as pulumi from "@pulumi/pulumi";
-
-// TODO: Integrate this into TechnoCore so that these env vars can be handled reasonably.
-// Or does a drone.io job make more sense?
-
-// https://www.pulumi.com/registry/packages/hcloud/
-// export HCLOUD_TOKEN=XXXXXXXXXXXXXX
 import * as hcloud from "@pulumi/hcloud";
 import {remote, types} from "@pulumi/command";
 
 // https://www.pulumi.com/registry/packages/cloudflare/
-// export CLOUDFLARE_API_TOKEN=YYYYYY
 import * as cloudflare from "@pulumi/cloudflare";
 
 import * as fs from "fs";
@@ -26,44 +19,46 @@ const privateKey = privateKeyBase64 ? Buffer.from(privateKeyBase64, 'base64').to
 
 // DNS Defaults
 // TODO: Move this into the config?
-const domain = 'ownyour.io'
-const ttl = 3600
+const defaultTTL = 60;
+const cloudflareProvider = new cloudflare.Provider('cloudflare-cloud', { apiToken: cloudflareToken });
 function createDnsRecords(server: hcloud.Server, dnsName: pulumi.Input<string>) {
     const ipv4Record = new cloudflare.Record(`${dnsName}-ipv4`, {
         name: dnsName,
-        zoneId: domain,
+        zoneId,
         type: "A",
         value: server.ipv4Address,
-        ttl: 3600
-    });
+        ttl: defaultTTL
+    }, {provider: cloudflareProvider});
 
     const ipv6Record = new cloudflare.Record(`${dnsName}-ipv6`, {
         name: dnsName,
-        zoneId: domain,
+        zoneId,
         type: "AAAA",
         value: server.ipv6Address,
-        ttl: 3600
-    });
+        ttl: defaultTTL
+    }, {provider: cloudflareProvider});
     
     return {ipv4Record, ipv6Record};
 }
 
 // TODO: Move this into the config?
 // VM Defaults
+const serverType = 'cpx11';
 const image = 'debian-11';
-const location = 'us-east';
+const location = 'ash';
 const serverType = 'cx11';
 const _default = new hcloud.SshKey("default", {publicKey: fs.readFileSync("~/.ssh/id_rsa.pub")});
 const sshKeys = [_default.id];
 
+const hcloudProvider = new hcloud.Provider('hetzner-cloud', { token: hcloudToken });
 const server = new hcloud.Server(`vpn.${domain}`, {
     serverType,
     image,
     location,
     sshKeys
-});
+}, {provider: hcloudProvider});
 
-const dnsRecords = ['dashboard', 'mx', 'vpn', 'api', 'broker'];
+const dnsRecords = ['dashboard.vpn', 'mx.vpn', 'vpn', 'api.vpn', 'broker.vpn'];
 dnsRecords.forEach((record: pulumi.Input<string>) => {
     createDnsRecords(server, record);
 });
@@ -74,23 +69,4 @@ const connection: types.input.remote.ConnectionArgs = {
     privateKey: privateKey,
 };
 
-new remote.Command("Install Docker", {
-    connection,
-    create: `
-    sudo apt-get update;
-    sudo apt-get install -y docker.io docker-compose wireguard
-    `,
-    // Might eventually want
-    // Something to do with TechnoCore: Install, configure, deploy
-    // Some kind of init process that adds the ./tc script to /usr/local/bin or something. Needs to stay up to date, maybe with ./tc update to update docker image(s)?
-        // Want to be able to run docker run  [--mount-docker-socket-options] scififarms/technocore:latest install data/ stack_template[Optional]
-        // edit data/.env
-        // tc deploy
-    // Configure TC backups.
-    // Auto Update nightly.
-    // Fail2ban
-    // lock down SSH
-    //sudo ufw allow proto tcp from any to any port 443 && sudo ufw allow 51821:51830/udp
-    //iptables --policy FORWARD ACCEPT
-    //delete: `rm private_ip.txt`,
-}, { deleteBeforeReplace: true });
+export const ipv4 = server.ipv4Address;
