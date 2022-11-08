9dad70b4155ed36f35b7f72f937bd7086dde5abd
Spencer Hachmeister <tms@spencerslab.com>
2022-11-07 20:35:03 -0700

    Add proxmox as a VM Provider

--- src/infra/index.ts
+++ src/infra/index.ts
@@ -13,24 +13,54 @@ import {
 import { remote, types } from "@pulumi/command";
 import { VirtualMachine, DNSRecord } from './resources';
 
-
 const config = new Config();
 const domain = config.require('domain');
-const hostname = config.get('vpn-hostname') ?? 'vpn';
 
-const vpnServer = new VirtualMachine('VPN Server', {
-    hostname,
+const templateProxmoxVM = new VirtualMachine(`debian-11-small-template`, {
+    hostname: `debian-11-small-template`,
+    domain: domain,
+    cloud: 'proxmox',
+    image: 'debian-11',
+    size: 'small',
+    debTemplate: true,
+    proxmoxTemplate: true,
+}, {
+});
+
+// Netmaker Server
+const vpnServerHostname = config.get('vpn-hostname') ?? 'vpn';
+//const vpnServer = new VirtualMachine('VPN Server', {
+//    hostname: vpnServerHostname,
+//    domain,
+//    additionalSubdomains: ['dashboard.vpn', 'mx.vpn', 'api.vpn', 'broker.vpn'],
+//    cloud: 'hetzner',
+//    size: 'small',
+//    dnsProvider: 'cloudflare',
+//    installDocker: true,
+//    installNetMaker: true,
+//    tlsEmail: 'tms@spencerslab.com',
+//}, {
+//
+//});
+
+// Netmaker ingress
+const vpnIngressHostname = config.get('vpn-ingress-hostname') ?? 'vpn-ingress';
+const vpnIngress = new VirtualMachine('VPN Ingress', {
+    hostname: vpnIngressHostname,
     domain,
-    additionalSubdomains: ['dashboard.vpn', 'mx.vpn', 'api.vpn', 'broker.vpn'],
-    cloud: 'hetzner',
+    cloud: 'proxmox',
     size: 'small',
     dnsProvider: 'cloudflare',
     installDocker: true,
-    installNetMaker: true,
+    installNetMaker: false,
+    installNetclient: true,
     tlsEmail: 'tms@spencerslab.com',
 }, {
-
+    dependsOn: [templateProxmoxVM]
 });
+
+export const vpnIngressIPv4 = vpnIngress.ipv4;
+export const vpnIngressFQDN = vpnIngress.fqdn;
 
 export const vpnIPv4 = vpnServer.ipv4;
 export const vpnFQDN = vpnServer.fqdn;
--- src/infra/resources/index.ts
+++ src/infra/resources/index.ts
@@ -2,10 +2,11 @@ import { ComponentResource, Output } from '@pulumi/pulumi';
 
 import {
     Config,
-    log, concat
+    log, concat, Input
 } from "@pulumi/pulumi";
 import * as hcloud from "@pulumi/hcloud";
 import * as cloudflare from "@pulumi/cloudflare";
+import * as proxmox from "@muhlba91/pulumi-proxmoxve";
 import { remote, types } from "@pulumi/command";
 
 import { readFileSync } from "fs";
@@ -18,7 +19,7 @@ export class VirtualMachine extends ComponentResource {
     constructor(
         name: string,
         args: {
-            dnsProvider: 'cloudflare' | 'hetzner';
+            dnsProvider?: 'cloudflare' | 'hetzner';
             cloud: 'proxmox' | 'hetzner';
             size: 'small' | 'medium' | 'large';
             additionalSubdomains?: string[];
@@ -26,23 +27,130 @@ export class VirtualMachine extends ComponentResource {
             domain?: string;
             installDocker?: boolean;
             installNetMaker?: boolean;
+            installNetclient?: boolean;
+            debTemplate?: boolean;
+            proxmoxTemplate?: boolean;
             tlsEmail?: string;
+            adminUser?: string;
+            image?: string;
+            vmId?: Input<number>;
         },
         opts: {},
     ) {
         super('pkg:index:VirtualMachine', name, {}, opts);
         this.fqdn = `${args.hostname}.${args.domain}`;
 
+        const image = args.image ?? config.get('default-image') ?? 'debian-11';
+
+        const adminUser = args.adminUser ?? config.get(`default-admin-user`) ?? 'admin';
         const publicKey = config.get(`${name}-publicKey`) ?? readFileSync(join(homedir(), ".ssh", "id_rsa.pub")).toString("utf8");
         const privateKey = config.getSecret(`${name}-privateKey`) ?? readFileSync(join(homedir(), ".ssh", "id_rsa")).toString("utf8");
+
+        const commandsDependsOn = [];
+
         switch (args.cloud) {
             case 'proxmox':
-                this.ipv4 = concat('');
-                this.ipv6 = concat('');
+
+                const proxmoxEndpoint = config.require('proxmox_ve_endpoint');
+                const provider = this.getProxmoxProvider(
+                    proxmoxEndpoint,
+                    true,
+                    config.require('proxmox_ve_username'),
+                    config.require('proxmox_ve_password')
+                );
+
+                let templateId: string;
+                let templateImageURL: string;
+                switch (image) {
+                    case 'debian-11':
+                        templateId = '999';
+                        templateImageURL = 'https://cdimage.debian.org/images/cloud/bullseye/latest/debian-11-nocloud-amd64.qcow2';
+                        if (args.proxmoxTemplate) {
+                            args.vmId = Number(templateId);
+                        }
+                        break;
+                    default:
+                        new Error(`image: ${image} not supported`);
+                        // This makes the linter happy. It should never get called.
+                        templateId = '';
+                        templateImageURL = '';
+                        break;
+                }
+
+                let preConditions = [provider];
+
+                const proxmoxServer = new proxmox.vm.VirtualMachine(`${this.fqdn}`, {
+                    nodeName: 'proxmox',
+                    agent: {
+                        enabled: true, // toggles checking for ip addresses through qemu-guest-agent
+                        trim: true,
+                        type: 'virtio',
+                    },
+                    cdrom: { enabled: true },
+                    cpu: {
+                        cores: 2,
+                        sockets: 1,
+                    },
+                    disks: [{
+                        interface: 'scsi0',
+                        datastoreId: 'local-lvm',
+                        size: 32,
+                        fileFormat: 'qcow2',
+                    }],
+                    initialization: {
+                        type: 'nocloud',
+                        dns: {
+                            domain: args.domain,
+                            server: '192.168.88.1',
+                        },
+                        datastoreId: 'local-lvm',
+                        userAccount: {
+                            username: adminUser,
+                            keys: [publicKey],
+                        }
+                    },
+                    memory: {
+                        dedicated: 2000,
+                    },
+                    name: args.hostname,
+                    networkDevices: [
+                        {
+                            bridge: 'vmbr0',
+                            model: 'virtio',
+                        },
+                    ],
+                    onBoot: true,
+                    operatingSystem: { type: 'l26' },
+                    started: true,
+                    timeoutShutdownVm: 45,
+                    timeoutReboot: 45,
+                    vmId: args.vmId,
+                }, { provider: provider, dependsOn: preConditions });
+
+                if (args.proxmoxTemplate) {
+                    const proxmoxHostRegexMatches = proxmoxEndpoint?.match(/http.*:\/\/(.*):/);
+                    const proxmoxHostname = proxmoxHostRegexMatches ? proxmoxHostRegexMatches[1] : '';
+
+                    const proxmoxConnection: types.input.remote.ConnectionArgs = {
+                        host: proxmoxHostname,
+                    };
+
+                    // Images can be found at: https://docs.openstack.org/image-guide/obtain-images.html
+                    const debTemplatePost = new remote.Command("Post-configure  Debian", {
+                        connection: proxmoxConnection,
+                        create: `
+                                            wget -O ${image}.qcow2 ${templateImageURL} 
+                                            qm importdisk ${templateId} ${image}.qcow2 local-lvm
+                                            `
+                    }, { dependsOn: [proxmoxServer] });
+                    commandsDependsOn.push(debTemplatePost);
+                }
+
+                this.ipv4 = proxmoxServer.ipv4Addresses[0][0];
+                this.ipv6 = proxmoxServer.ipv6Addresses[0][0];
                 break;
             case 'hetzner':
                 const serverType = config.get(`hetzner-vm-${args.size}`) ?? 'cpx11';
-                const image = config.get('hetzner-default-image') ?? 'debian-11';
                 const location = config.get('hetzner-default-location') ?? 'ash';
 
                 const hcloudKey = new hcloud.SshKey(`ssh-${this.fqdn}`, {
@@ -50,7 +158,7 @@ export class VirtualMachine extends ComponentResource {
                 });
                 const sshKeys = [hcloudKey.id];
 
-                const server = new hcloud.Server(`${args.hostname}.${args.domain}`, {
+                const server = new hcloud.Server(`${this.fqdn}`, {
                     serverType,
                     image,
                     location,
@@ -58,29 +166,30 @@ export class VirtualMachine extends ComponentResource {
                 }, {});
                 this.ipv4 = server.ipv4Address;
                 this.ipv6 = server.ipv6Address;
-
-                new DNSRecord(this.fqdn, {
-                    dnsProvider: 'cloudflare',
-                    ipv4: this.ipv4,
-                    ipv6: this.ipv6,
-                }, {});
-
-                if (args.additionalSubdomains) {
-                    args.additionalSubdomains.forEach((record: string) => {
-                        new DNSRecord(`${record}.${args.domain}`, {
-                            dnsProvider: 'cloudflare',
-                            ipv4: this.ipv4,
-                            ipv6: this.ipv6,
-                        }, {});
-                    });
-                }
-
                 break;
+        }
+
+        if (args.dnsProvider) {
+            new DNSRecord(this.fqdn, {
+                dnsProvider: 'cloudflare',
+                ipv4: this.ipv4,
+                ipv6: this.ipv6,
+            }, {});
+
+            if (args.additionalSubdomains) {
+                args.additionalSubdomains.forEach((record: string) => {
+                    new DNSRecord(`${record}.${args.domain}`, {
+                        dnsProvider: 'cloudflare',
+                        ipv4: this.ipv4,
+                        ipv6: this.ipv6,
+                    }, {});
+                });
+            }
         }
 
         const connection: types.input.remote.ConnectionArgs = {
             host: this.ipv4,
-            user: "root",
+            user: adminUser,
             privateKey: privateKey,
         };
 
@@ -95,7 +204,7 @@ export class VirtualMachine extends ComponentResource {
                     apt-get install -y docker-compose;
                     systemctl enable --now docker;
             `,
-            }, { deleteBeforeReplace: true });
+            }, { deleteBeforeReplace: true, dependsOn: commandsDependsOn });
 
             const netmaker = new remote.Command("Install Netmaker", {
                 connection,
@@ -139,6 +248,24 @@ export class VirtualMachine extends ComponentResource {
             }, { dependsOn: [dockerCompose, netmaker, mosquitto], deleteBeforeReplace: true });
         }
     }
+
+    private static proxmoxProvider: proxmox.Provider | undefined;
+    getProxmoxProvider(endpoint: string, insecure: boolean, username: string, password: string): proxmox.Provider {
+        // Have to use a provider to work around a nested config issue
+        // https://www.pulumi.com/registry/packages/proxmoxve/installation-configuration/
+        if (!VirtualMachine.proxmoxProvider) {
+            VirtualMachine.proxmoxProvider = new proxmox.Provider('proxmoxve', {
+                virtualEnvironment: {
+                    endpoint: endpoint,
+                    insecure: insecure,
+                    username: username,
+                    password: password,
+                }
+            }, {});
+        }
+        return VirtualMachine.proxmoxProvider;
+    }
+
     fqdn: string;
     ipv4: Output<string>;
     ipv6: Output<string>;
