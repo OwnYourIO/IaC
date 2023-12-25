d4c60ee32408332df8cf0841c7eed5145dc51d64
Spencer Hachmeister <tms@spencerslab.com>
2023-12-25 08:42:07 -0700

    Finish getting core working consistently.

--- projects/core/index.ts
+++ projects/core/index.ts
@@ -53,6 +53,8 @@ k3sVM.run('install-argocd', {
         #helm repo add jetstack https://charts.jetstack.io
 
         ${k3sVM.sudo} -E helm install base OwnYourIO/deploy
+        # Give the resources a chance to be created otherwise it just fails right away.
+        sleep 15
         ${k3sVM.sudo} -E $(which kubectl) wait --namespace default --for=condition=ready pod --selector=app.kubernetes.io/name=argocd-server --timeout=120s
         ${k3sVM.sudo} -E $(which kubectl) wait --namespace default --for=condition=ready pod --selector=app.kubernetes.io/name=argocd-application-controller --timeout=120s
         ${k3sVM.sudo} -E $(which kubectl) wait --namespace default --for=condition=ready pod --selector=app.kubernetes.io/name=argocd-repo-server --timeout=120s
@@ -69,6 +71,7 @@ k3sVM.run('configure-argocd', {
 
         ${k3sVM.sudo} -E bash -c "
             echo ${config.get('sealed-secret-key')} | base64 -d - | $(which kubectl) create --namespace default -f -
+            echo ${config.get('bitwarden-cli-secret')} | base64 -d - | $(which kubectl) create --namespace default -f -
 
             echo y | $(which kubectl) exec -i svc/base-argocd-server -- argocd login 'localhost:8080'  --username=admin --password=$($(which kubectl) exec svc/base-argocd-server -- argocd admin initial-password | head -n 1) --insecure
             $(which kubectl) exec svc/base-argocd-server -- argocd cluster set in-cluster --name ${k3sVM.hostname}
@@ -77,8 +80,8 @@ k3sVM.run('configure-argocd', {
             $(which kubectl) annotate secret -l argocd.argoproj.io/secret-type=cluster 'repo.chart=${chartPath}'
             $(which kubectl) annotate secret -l argocd.argoproj.io/secret-type=cluster repo.chart.path=charts/
             $(which kubectl) annotate secret -l argocd.argoproj.io/secret-type=cluster repo.values=https://github.com/OwnYourIO/IaC.git
-            $(which kubectl) annotate secret -l argocd.argoproj.io/secret-type=cluster repo.values.path=src/projects/
-            $(which kubectl) patch clusterrole base-argocd-server --type='json' -p='[{\\"op\\": \\"add\\", \\"path\\": \\"/rules/0\\", \\"value\\":{ \\"apiGroups\\": [\\"\\"], \\"resources\\": [\\"applicationsets\\"], \\"verbs\\": [\\"create\\",\\"patch\\"]}}]'
+            $(which kubectl) annotate secret -l argocd.argoproj.io/secret-type=cluster repo.values.path=projects/
+            $(which kubectl) patch clusterrole base-argocd-server --type='json' -p='[{\\"op\\": \\"add\\", \\"path\\": \\"/rules/0\\", \\"value\\":{ \\"apiGroups\\": [\\"argoproj.io\\"], \\"resources\\": [\\"applicationsets\\"], \\"verbs\\": [\\"create\\",\\"patch\\"]}}]'
 
             $(which kubectl) exec svc/base-argocd-server -- argocd appset create '${appSetPath}'
         "
